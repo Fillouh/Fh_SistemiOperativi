09962ed8d58e0d0059feff75c5bc55522333a6dc
Filippo Leonelli <101930548+Fillouh@users.noreply.github.com>
2024-02-21 18:43:50 +0100

    Create FCP.sh

---
+++ Shell/Prova 12-09-2012/FCP.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+echo QUESTA E\' LA PROVA DEL 12 SETTEMBRE DEL 2012
+echo FILIPPO LEONELLI `date`
+
+#controllo sul numero dei parametri
+if test $# -ne 2
+then
+	echo NUMERO DI PARAMETRI PASSATI NON CORRETTO
+	exit 1
+fi
+
+#controllo sul tipo dei parametri
+G=$1
+C=$2
+
+#controllo sul primo parametro
+case $G in 
+	/*) if test -d $G -a -x $G
+        then
+        	echo PARAMETRO 1 OK
+        else
+        	echo PARAMETRO 1 NOT OK
+        	exit 2
+        fi
+        ;;
+     *)echo PARAMETRO 1 NOT OK
+        exit 3
+        ;;
+esac
+
+#controllo sul secondo parametro
+case $C in
+	?) case $C in
+         [a-z,A-Z])echo PARAMETRO 2 OK
+                   ;;
+              *)echo PARAMETRO 2 NOT OK
+             exit 4
+             ;;
+        esac
+       ;;
+
+    *) echo PARAMETRO 2 NOT OK
+       exit 4
+             ;;
+esac
+
+#impostazione per la chiamata ricorsiva
+PATH=`pwd`:$PATH
+export PATH
+>/tmp/file
+
+#chiamata al file ricorsivo
+FCR.sh $G $C /tmp/file
+
+#comandi post chiamata al file ricorsivo
+for i in `cat /tmp/file`
+do 
+	echo FILE : $i
+	echo CONTENUTO DEL FILE
+	cat $i
+done
+
+rm /tmp/file
+
+echo HO FINITO TUTTO
